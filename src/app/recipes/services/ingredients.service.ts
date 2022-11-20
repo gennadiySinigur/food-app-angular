e81ef9f517e378d558d3e254a7851bff79d32ccb
gennadiySinigur <gennadiysinigur@gmail.com>
2022-11-20 03:12:35 +0200

    Replaced extract method functionality with rxjs

--- src/app/recipes/services/ingredients.service.ts
+++ src/app/recipes/services/ingredients.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@angular/core';
+import {
+  from,
+  map,
+  of,
+  skip,
+  take,
+  zip
+} from 'rxjs';
+
 import { Ingredient } from '../models/ingredient';
 import { RecipeDetailsInfo } from '../models/recipe-details-info';
 
@@ -11,31 +20,70 @@ export class IngredientsService {
   extract(mealsData: RecipeDetailsInfo) {
     const mealIngredients: Array<Ingredient> = [];
 
-    for (let [key, value] of Object.entries(mealsData)) {
-      if (key.includes('strMeasure')) {
-        mealIngredients.push({
-          measure: value
-        })
-      }
+    of(Object.entries(mealsData)).pipe(
+      map(data => {
+        data.map(
+          ([key, value]) => {
+            if (key.includes('strMeasure')) {
+              mealIngredients.push({ measure: value });
+            }
 
-      if (key.includes('strIngredient')) {
-        mealIngredients.push({
-          name: value,
-        });
-      }
-    }
+            if (key.includes('strIngredient')) {
+              mealIngredients.push({ name: value });
+            }
+          }
+        );
+      })
+    ).subscribe();
 
-    let ingredients = [];
-    let measureIndex = 20;
+    /** Without RxJs
+     *
+     * for (let [key, value] of Object.entries(mealsData)) {
+     *    if (key.includes('strMeasure')) {
+     *      mealIngredients.push({
+     *        measure: value
+     *      })
+     *    }
+     *
+     *    if (key.includes('strIngredient')) {
+     *      mealIngredients.push({
+     *        name: value,
+     *      });
+     *    }
+     *  }
+     */
 
-    for (let i = 0; i < mealIngredients.length; i++) {
-      ingredients.push({
-        name: mealIngredients[i].name,
-        measure: mealIngredients[measureIndex]?.measure,
-      });
+    let ingredients: Array<Ingredient> = [];
 
-      measureIndex++;
-    }
+    const names$ = from(mealIngredients)
+      .pipe(
+        take(20),
+        map(value => value.name),
+      );
+
+    const measures$ = from(mealIngredients)
+      .pipe(
+        skip(20),
+        map(value => value.measure),
+      )
+
+    zip(names$, measures$).pipe(
+      map(([name, measure]) => ({ name, measure }))
+    ).subscribe(value => ingredients.push(value));
+
+    /** Without RxJs
+     *
+     *  let measureIndex = 20;
+     *
+     *  for (let i = 0; i < mealIngredients.length; i++) {
+     *    ingredients.push({
+     *      name: mealIngredients[i].name,
+     *      measure: mealIngredients[measureIndex]?.measure,
+     *    });
+     *
+     *  measureIndex++;
+     }
+     **/
 
     this.ingredients = ingredients;
   }
