c3de4532bde8b0721bdd3aa65ea3102692cc28f7
gennadiySinigur <gennadiysinigur@gmail.com>
2023-01-14 17:52:04 +0200

    Initialized Add recipe form

--- src/app/recipes/components/add-recipe-form/add-recipe-form.component.ts
+++ src/app/recipes/components/add-recipe-form/add-recipe-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormArray, FormBuilder } from '@angular/forms';
+
+import { MyRecipesService } from '../../services/my-recipes.service';
 
 @Component({
   selector: 'app-add-recipe-form',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-recipe-form.component.scss']
 })
 export class AddRecipeFormComponent implements OnInit {
+  addRecipeForm!: FormGroup;
+  ingredients: FormArray = new FormArray<FormControl>([new FormControl()]);
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private myRecipesService: MyRecipesService
+  ) {
+    this.ingredients = this.formBuilder.array([
+      this.formBuilder.group({
+        name: [''],
+        measure: ['']
+      })
+    ]);
   }
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.addRecipeForm = new FormGroup({
+      title: new FormControl(null, [
+        Validators.required
+      ]),
+      category: new FormControl(null, [
+        Validators.required
+      ]),
+      imageAddress: new FormControl(null, [
+        Validators.required
+      ]),
+      ingredients: this.ingredients,
+      instruction: new FormControl(null, [
+        Validators.required
+      ])
+    });
+  }
+
+  addIngredient(): void {
+    this.ingredients.push(this.formBuilder.group({
+      name: [''],
+      measure: ['']
+    }));
+  }
+
+  submit() {
+    if (this.addRecipeForm.invalid) {
+      return;
+    }
+
+    console.log('form', this.addRecipeForm.value);
+
+    this.myRecipesService.save(this.addRecipeForm.value);
+  }
 }
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLinkWithHref } from '@angular/router';
+
 import { RecipesComponent } from './recipes.component';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
-import { RouterLinkWithHref } from '@angular/router';
 import { RecipeCategoriesComponent } from './components/recipe-categories/recipe-categories.component';
 import { MyRecipesComponent } from './components/my-recipes/my-recipes.component';
 import { AddRecipeFormComponent } from './components/add-recipe-form/add-recipe-form.component';
@@ -22,6 +24,7 @@ import { AddRecipeFormComponent } from './components/add-recipe-form/add-recipe-
   imports: [
     CommonModule,
     RouterLinkWithHref,
+    ReactiveFormsModule,
   ],
   exports: [
     RecipesComponent,
