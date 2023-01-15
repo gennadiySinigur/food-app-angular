085af57c52401261ee065d05e347d1acedb2e12a
gennadiySinigur <gennadiysinigur@gmail.com>
2023-01-15 13:22:33 +0200

    Updated Recipe Details component for displaying My Recipe Details as well

--- src/app/recipes/components/recipe-details/recipe-details.component.ts
+++ src/app/recipes/components/recipe-details/recipe-details.component.ts
@@ -9,6 +9,8 @@ import { Ingredient } from '../../models/ingredient';
 import { RecipesService } from '../../services/recipes.service';
 import { RecipeDetailsInfo } from '../../models/recipe-details-info';
 import { TransformResponseDataService } from '../../services/transform-response-data.service';
+import { MyRecipesService } from '../../services/my-recipes.service';
+import { MyRecipeWithId } from '../../models/my-recipe-with-id';
 
 @Component({
   selector: 'app-recipe-details',
@@ -17,6 +19,9 @@ import { TransformResponseDataService } from '../../services/transform-response-
 })
 export class RecipeDetailsComponent implements OnInit {
   recipeInfo!: RecipeDetailsInfo;
+  myRecipeInfo!: MyRecipeWithId;
+  currentPath: string = '';
+  isMyRecipe = false;
 
   recipeIngredients: Array<Ingredient> = [];
 
@@ -24,11 +29,26 @@ export class RecipeDetailsComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private recipesService: RecipesService,
-    private transformResponseDataService: TransformResponseDataService
+    private transformResponseDataService: TransformResponseDataService,
+    private myRecipesService: MyRecipesService
   ) { }
 
   ngOnInit(): void {
+    this.getCurrentPath();
+
+    if (this.currentPath.includes('my-recipes')) {
+      this.getMyRecipeInfo();
+
+      return;
+    }
+
     this.getRecipeInfo();
+  }
+
+  getCurrentPath() {
+    this.activatedRoute.url.subscribe(url => {
+      this.currentPath = url.join('/');
+    });
   }
 
   getRecipeInfo(): void {
@@ -43,5 +63,19 @@ export class RecipeDetailsComponent implements OnInit {
         this.recipeInfo = recipeData;
       }
     );
+
+    this.isMyRecipe = false;
+  }
+
+  getMyRecipeInfo(): void {
+    this.activatedRoute.paramMap.pipe(
+      switchMap((params: ParamMap): Observable<MyRecipeWithId> => {
+        return this.myRecipesService.getById(params.get('id')!);
+      })
+    ).subscribe((recipeData: MyRecipeWithId) => {
+      this.myRecipeInfo = recipeData;
+    });
+
+    this.isMyRecipe = true;
   }
 }
