c93c26e24773fa89381d14dac685ecab0c92505f
gennadiySinigur <gennadiysinigur@gmail.com>
2023-04-18 17:47:26 +0300

    Moved Header to Shared module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterLinkActive, RouterLinkWithHref} from '@angular/router';
 
-import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -10,23 +8,17 @@ import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     HomeComponent,
     AboutComponent,
     RegistrationComponent,
-    LoginComponent
+    LoginComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterLinkWithHref,
-    RouterLinkActive
-  ],
+  imports: [CommonModule],
   exports: [
-    HeaderComponent,
     HomeComponent,
     AboutComponent,
     RegistrationComponent,
-    LoginComponent
-  ]
+    LoginComponent,
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ToastComponent } from './toast/toast.component';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    ToastComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    FooterComponent,
-    ToastComponent
-  ]
+  declarations: [HeaderComponent, FooterComponent, ToastComponent],
+  imports: [CommonModule, RouterLinkWithHref, RouterLinkActive],
+  exports: [HeaderComponent, FooterComponent, ToastComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
