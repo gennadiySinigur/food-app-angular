e02ae7912e68919fe0921b2f41baa28314b9776c
gennadiySinigur <gennadiysinigur@gmail.com>
2023-04-18 16:42:49 +0300

    Added AuthModule into the AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,20 +10,24 @@ import { SharedModule } from './shared/shared.module';
 import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
     SharedModule,
-    RecipesModule
+    RecipesModule,
+    AuthModule.forRoot({
+      domain: 'dev-cr3yhps5x3dkfvni.us.auth0.com',
+      clientId: 'PdKBDfUamHRGwcjT9hJdXCGmBKk3slUo',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
   providers: [],
-  exports: [
-  ],
-  bootstrap: [AppComponent]
+  exports: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
