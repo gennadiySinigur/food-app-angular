497102413a09dcdce16f34cfba0add84b56a19d1
gennadiySinigur <gennadiysinigur@gmail.com>
2023-04-18 16:43:52 +0300

    Added login(), logout() methods

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 import { BlockUIService } from '../../shared/services/block-ui.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(
+    protected blockUIService: BlockUIService,
+    public authService: AuthService
+  ) {}
 
-  constructor(protected blockUIService: BlockUIService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this.authService.loginWithRedirect();
   }
 
+  logout() {
+    this.authService.logout({
+      logoutParams: {
+        returnTo: document.location.origin,
+      },
+    });
+  }
 }
